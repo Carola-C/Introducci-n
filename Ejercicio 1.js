199d6ade175806dc204fc3d98fb0ddf63de34d40
Carola-C <e_mo04@outlook.com>
2023-12-09 17:01:48 -0600

    first commit

---
+++ Ejercicio 1.js
@@ -0,0 +1,20 @@
+var automovil = "Chevrolet";
+console.log(automovil);
+var kilometro = 300;
+console.log(kilometro);
+var mantenimiento = true;
+console.log(mantenimiento);
+var propiedarios = ['John', 'Alberto','Cristian','Jose'];
+console.log(propiedarios);
+var computador = {
+    procesador: 'intel',
+    ram: 8,
+    memoria: '1TB',
+    funcional: true
+};
+console.log(computador);
+console.log(typeof automovil);
+console.log(typeof kilometro);
+console.log(typeof mantenimiento);
+console.log(typeof propiedarios);
+console.log(typeof computador);
